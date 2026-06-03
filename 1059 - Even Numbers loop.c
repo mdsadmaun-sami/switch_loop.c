f581361cf935d5c42e7388f26727cfb90b950df8
MD SAD MAUN SAMI <mdsadmaunsami@gmail.com>
2026-06-03 23:23:00 +0600

    Refactor even numbers loop for efficiency

---
+++ 1059 - Even Numbers loop.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    for ( int i = 1; i <= 100 ; i++ )
+    {
+        if ( i % 2 == 0 )
+        {
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
+
+-------------------------------------------
+
+#include<stdio.h>
+int main()
+{
+    for ( int i = 2; i <= 100 ; i+=2 )
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
